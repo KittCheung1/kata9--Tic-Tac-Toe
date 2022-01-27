91f94a5b9926d0d9c61dd47708df4daf1afe141c
Kitt Cheung <kitt.cheung@se.experis.com>
2022-01-27 10:27:46 +0100

    kata 9: tic-tac-toe not working

---
+++ app.js
@@ -0,0 +1,36 @@
+function ticTacToe(input) {
+    const arr1 = []
+    const arr2 = []
+    const arr3 = []
+    for (let i = 0; i < 3; i++) {
+        arr1.push(input[i])
+        for (let j = 0; j < input.length; j++) {
+        }
+    }
+    console.log(arr1[0])
+
+    input.forEach(element => {
+        arr.push(element);
+        console.log(arr);
+    });
+    // const arr1 = []
+    // const arr2 = []
+    // const arr3 = []
+
+    // arr1.push(input[0])
+    // arr2.push(input[1])
+    // arr3.push(input[2])
+
+    // console.log(arr1)
+    // for (let i = 0; i < arr1.length; i++) {
+    //     const element = array[i];
+        
+        
+    // }
+
+
+}
+
+ticTacToe([["X", "O", "X"], ["O", "X", "O"], ["O", "X", "X"]
+
+])
